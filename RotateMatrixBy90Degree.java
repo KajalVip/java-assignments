77d6851f25084316fac060c50cee83a574a89934
KajalVip <130542409+KajalVip@users.noreply.github.com>
2026-05-25 11:26:42 +0530

    Add files via upload
    write a program to rotate matrix 90 degree. input matrix=
     1 2 3
     4 5 6
     7 8 9
     output= 
     7 4 1
     8 5 2
     9 6 3

---
+++ RotateMatrixBy90Degree.java
@@ -0,0 +1,38 @@
+public class RotateMatrixBy90Degree {
+    public static void main(String[] args) {
+        int [][]matrix={
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        int n=matrix.length;
+        //transpose matrix
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        //reverse matrix
+        for(int i=0;i<n;i++){
+            int left=0;
+            int right=n-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+
+            }
+        }
+        //print
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
